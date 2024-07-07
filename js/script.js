a57603e8ced60b445baee17eae82b004a8239f75
Kate Veremeychik <kateiiverem@gmail.com>
2024-07-08 00:46:27 +0300

    fix: style of callback form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const isMobile = {
   Android: function () {
     return navigator.userAgent.match(/Android/i);
@@ -298,5 +300,21 @@ if (width < 768) {
     }
   });
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('form');
+  form.addEventListener('submit', formSend);
+
+  async function formSend(e) {
+    e.preventDefault;
+
+    let error = formValidate(form);
+  }
+
+  function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll('._req');
+  }
+})
 
 pageSlider.init();
