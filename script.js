77479217293dc6f51d66a2c9fc89674b239b87eb
Kate Veremeychik <kateiiverem@gmail.com>
2024-07-09 16:19:15 +0300

    fix: add logic for get data without server

--- script.js
+++ script.js
@@ -304,78 +304,78 @@ if (width < 768) {
   });
 }
 
-// document.getElementById('form').addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   const formData = Object.fromEntries(new FormData(e.target).entries());
-//   console.log(formData);
-//   form.reset();
-// })
+document.getElementById('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = Object.fromEntries(new FormData(e.target).entries());
+  console.log(formData);
+  form.reset();
+})
 
 
-form.addEventListener('submit', formSend);
+// form.addEventListener('submit', formSend);
 
-async function formSend(e) {
-  e.preventDefault;
+// async function formSend(e) {
+//   e.preventDefault;
 
-  let error = formValidate(form);
+//   let error = formValidate(form);
 
-  let formData = new FormData(form);
+//   let formData = new FormData(form);
 
-  if (error === 0) {
-    form.classList.add('_sending');
-    let response = await fetch('sendmail.php', {
-      method: 'POST',
-      body: formData,
-    });
+//   if (error === 0) {
+//     form.classList.add('_sending');
+//     let response = await fetch('sendmail.php', {
+//       method: 'POST',
+//       body: formData,
+//     });
 
-    if (response.ok) {
-      let result = await response.json();
-      alert(result.message);
-      form.reset();
-      form.classList.remove('_sending');
-    } else {
-      // alert('Error');
-      form.classList.remove('_sending');
-    }
-  } else {
-    alert('Fill required fields');
-  }
-}
-function formValidate(form) {
-  let error = 0;
-  let formReq = document.querySelectorAll('._req');
-  for (let index = 0; index < formReq.length; index++) {
-    const input = formReq[index];
-    formRemoveError(input);
+//     if (response.ok) {
+//       let result = await response.json();
+//       alert(result.message);
+//       form.reset();
+//       form.classList.remove('_sending');
+//     } else {
+//       // alert('Error');
+//       form.classList.remove('_sending');
+//     }
+//   } else {
+//     alert('Fill required fields');
+//   }
+// }
+// function formValidate(form) {
+//   let error = 0;
+//   let formReq = document.querySelectorAll('._req');
+//   for (let index = 0; index < formReq.length; index++) {
+//     const input = formReq[index];
+//     formRemoveError(input);
 
-    if (input.classList.contains('_email')) {
-      if (emailTest(input)) {
-        formAddError(input);
-        error++;
-      }
-    } else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
-      formAddError(input);
-      error++;
-    } else {
-      if (input.value === '') {
-        formAddError(input);
-        error++;
-      }
-    }
-  }
-  return error;
-}
-function formAddError(input) {
-  input.parentElement.classList.add('_error');
-  input.classList.add('_error');
-}
-function formRemoveError(input) {
-  input.parentElement.classList.remove('_error');
-  input.classList.remove('_error');
-}
-function emailTest(input) {
-  return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-}
+//     if (input.classList.contains('_email')) {
+//       if (emailTest(input)) {
+//         formAddError(input);
+//         error++;
+//       }
+//     } else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+//       formAddError(input);
+//       error++;
+//     } else {
+//       if (input.value === '') {
+//         formAddError(input);
+//         error++;
+//       }
+//     }
+//   }
+//   return error;
+// }
+// function formAddError(input) {
+//   input.parentElement.classList.add('_error');
+//   input.classList.add('_error');
+// }
+// function formRemoveError(input) {
+//   input.parentElement.classList.remove('_error');
+//   input.classList.remove('_error');
+// }
+// function emailTest(input) {
+//   return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+// }
 
 
 
