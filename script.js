f02fb737a341f39e627cc430f22a4d584c666cdb
Catherine <60444425+MonkeyKingBar-bit@users.noreply.github.com>
2024-07-23 17:23:53 +0300

    add: logic of send request from site to email

--- script.js
+++ script.js
@@ -1,29 +1,119 @@
-'use strict'
+"use strict";
 
-const wrapper = document.querySelector('.wrapper');
-const menuLinks = document.querySelectorAll('.menu__link');
-const benefitItems = document.querySelectorAll('.benefit-item');
-// const solutionItems = document.querySelectorAll('.solutions__item');
-const solutionItem1 = document.querySelector('.solutions__list li:nth-child(1)');
-const solutionItem2 = document.querySelector('.solutions__list li:nth-child(2)');
-const solutionItem3 = document.querySelector('.solutions__list li:nth-child(3)');
-const solutionItem4 = document.querySelector('.solutions__list li:nth-child(4)');
-const solutionItem5 = document.querySelector('.solutions__list li:nth-child(5)');
-const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.menu__body');
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", formSend);
 
+  async function formSend(e) {
+    e.preventDefault();
+
+    let error = formValidate(form);
+
+    let formData = new FormData(form);
+
+    if (error === 0) {
+      form.classList.add("_sending");
+
+      let response = await fetch("sendmail.php", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        let result = await response.json();
+        alert(result.message);
+        form.reset();
+        form.classList.remove("_sending");
+      } else {
+        alert("Error");
+        form.classList.remove("_sending");
+      }
+    } else {
+      alert("Fill in required fields");
+    }
+  }
+
+  function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll("._req");
+
+    for (let index = 0; index < formReq.length; index++) {
+      const input = formReq[index];
+      formRemoveError(input);
+
+      if (input.classList.contains("_email")) {
+        if (emailTest(input)) {
+          formAddError(input);
+          error++;
+        }
+      } else {
+        if (input.value === "") {
+          formAddError(input);
+          error++;
+        }
+      }
+
+      // if (input.classList.contains("_phone")) {
+      //   if (isPhoneValid(input)) {
+      //     formAddError(input);
+      //     error++;
+      //   }
+      // }
+    }
+    return error;
+  }
+
+  function formAddError(input) {
+    input.parentElement.classList.add("_error");
+    input.classList.add("_error");
+  }
+
+  function formRemoveError(input) {
+    input.parentElement.classList.remove("_error");
+    input.classList.remove("_error");
+  }
+
+  function emailTest(input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?w+)*(\.\w{2,8})+$/.test(input.value);
+  }
+
+  // function isPhoneValid(input) {
+  //   return !/^\8\d{3}\d{3}\d{2}\d{2}$/g.test(input.value);
+  // }
+});
+
+const wrapper = document.querySelector(".wrapper");
+const menuLinks = document.querySelectorAll(".menu__link");
+const benefitItems = document.querySelectorAll(".benefit-item");
+const solutionItem1 = document.querySelector(
+  ".solutions__list li:nth-child(1)"
+);
+const solutionItem2 = document.querySelector(
+  ".solutions__list li:nth-child(2)"
+);
+const solutionItem3 = document.querySelector(
+  ".solutions__list li:nth-child(3)"
+);
+const solutionItem4 = document.querySelector(
+  ".solutions__list li:nth-child(4)"
+);
+const solutionItem5 = document.querySelector(
+  ".solutions__list li:nth-child(5)"
+);
+const iconMenu = document.querySelector(".menu__icon");
+const menuBody = document.querySelector(".menu__body");
 
 if (iconMenu) {
-  iconMenu.addEventListener('click', function () {
-    document.body.classList.toggle('_lock');
-    iconMenu.classList.toggle('_active');
-    menuBody.classList.toggle('_active');
+  iconMenu.addEventListener("click", function () {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
   });
 }
 
-let pageSlider = new Swiper('.page', {
-  direction: 'vertical',
-  slidesPerView: 'auto',
+let pageSlider = new Swiper(".page", {
+  direction: "vertical",
+  slidesPerView: "auto",
   parallax: true,
   keyboard: {
     enabled: true,
@@ -39,15 +129,15 @@ let pageSlider = new Swiper('.page', {
   observerParents: true,
   observeSlideChildren: true,
   pagination: {
-    el: '.page__pagination',
-    type: 'bullets',
+    el: ".page__pagination",
+    type: "bullets",
     clickable: true,
-    bulletClass: 'page__bullet',
-    bulletActiveClass: 'page__bullet_active',
+    bulletClass: "page__bullet",
+    bulletActiveClass: "page__bullet_active",
   },
   scrollbar: {
-    el: '.page__scroll',
-    dragClass: 'page__drag-scroll',
+    el: ".page__scroll",
+    dragClass: "page__drag-scroll",
     draggable: true,
   },
   init: false,
@@ -55,20 +145,19 @@ let pageSlider = new Swiper('.page', {
     init: function () {
       menuSlider();
       setScrollType();
-      wrapper.classList.add('_loaded');
+      wrapper.classList.add("_loaded");
     },
     slideChange: function () {
       menuSliderRemove();
-      menuLinks[pageSlider.realIndex].classList.add('_active');
-      benefitItems.forEach(benefitItem => {
-        benefitItem.classList.add('_active');
+      menuLinks[pageSlider.realIndex].classList.add("_active");
+      benefitItems.forEach((benefitItem) => {
+        benefitItem.classList.add("_active");
       });
       animSolution();
-
     },
     resize: function () {
       setScrollType();
-    }
+    },
   },
   breakpoints: {
     320: {
@@ -76,82 +165,98 @@ let pageSlider = new Swiper('.page', {
     },
     640: {
       parallax: true,
-    }
-  }
+    },
+  },
 });
 
 function animSolution() {
-  solutionItem1.animate({
-    opacity: [0, 1],
-    transform: ["translate(-120%, 0px)", "translate(0px, 0px)"],
-    transition: "all 0.8s ease 0s",
-  }, {
-    duration: 2000,
-    animationTimingFunction: 'ease',
-    animationFillMode: 'forwards',
-    animationDelay: '0.5s',
-  });
-  solutionItem2.animate({
-    opacity: [0, 1],
-    transform: ["translate(120%, 0px)", "translate(0px, 0px)"],
-    transition: "all 0.8s ease 0.2s"
-  }, {
-    duration: 2000,
-    animationTimingFunction: 'ease',
-    animationFillMode: 'forwards',
-    animationDelay: '0.7s',
-  });
-  solutionItem3.animate({
-    opacity: [0, 1],
-    transform: ["translate(-120%, 0px)", "translate(0px, 0px)"],
-    transition: "all 0.8s ease 0.4s"
-  }, {
-    duration: 2000,
-    animationTimingFunction: 'ease',
-    animationFillMode: 'forwards',
-    animationDelay: '0.9s',
-  });
-  solutionItem4.animate({
-    opacity: [0, 1],
-    transform: ["translate(120%, 0px)", "translate(0px, 0px)"],
-    transition: "all 0.8s ease 0.6s"
-  }, {
-    duration: 2000,
-    animationTimingFunction: 'ease',
-    animationFillMode: 'forwards',
-    animationDelay: '1.1s',
-  });
-  solutionItem5.animate({
-    opacity: [0, 1],
-    transform: ["translate(-120%, 0px)", "translate(0px, 0px)"],
-    transition: "all 0.8s ease 0.8s"
-  }, {
-    duration: 2000,
-    animationTimingFunction: 'ease',
-    animationFillMode: 'forwards',
-    animationDelay: '1.3s',
-  });
+  solutionItem1.animate(
+    {
+      opacity: [0, 1],
+      transform: ["translate(-120%, 0px)", "translate(0px, 0px)"],
+      transition: "all 0.8s ease 0s",
+    },
+    {
+      duration: 2000,
+      animationTimingFunction: "ease",
+      animationFillMode: "forwards",
+      animationDelay: "0.5s",
+    }
+  );
+  solutionItem2.animate(
+    {
+      opacity: [0, 1],
+      transform: ["translate(120%, 0px)", "translate(0px, 0px)"],
+      transition: "all 0.8s ease 0.2s",
+    },
+    {
+      duration: 2000,
+      animationTimingFunction: "ease",
+      animationFillMode: "forwards",
+      animationDelay: "0.7s",
+    }
+  );
+  solutionItem3.animate(
+    {
+      opacity: [0, 1],
+      transform: ["translate(-120%, 0px)", "translate(0px, 0px)"],
+      transition: "all 0.8s ease 0.4s",
+    },
+    {
+      duration: 2000,
+      animationTimingFunction: "ease",
+      animationFillMode: "forwards",
+      animationDelay: "0.9s",
+    }
+  );
+  solutionItem4.animate(
+    {
+      opacity: [0, 1],
+      transform: ["translate(120%, 0px)", "translate(0px, 0px)"],
+      transition: "all 0.8s ease 0.6s",
+    },
+    {
+      duration: 2000,
+      animationTimingFunction: "ease",
+      animationFillMode: "forwards",
+      animationDelay: "1.1s",
+    }
+  );
+  solutionItem5.animate(
+    {
+      opacity: [0, 1],
+      transform: ["translate(-120%, 0px)", "translate(0px, 0px)"],
+      transition: "all 0.8s ease 0.8s",
+    },
+    {
+      duration: 2000,
+      animationTimingFunction: "ease",
+      animationFillMode: "forwards",
+      animationDelay: "1.3s",
+    }
+  );
 }
-const animItems = document.querySelectorAll('._anim-items');
+const animItems = document.querySelectorAll("._anim-items");
 if (animItems.length > 0) {
-  window.addEventListener('scroll', animOnScroll);
+  window.addEventListener("scroll", animOnScroll);
   function animOnScroll() {
     for (let index = 0; index < animItems.length; index++) {
       const animItem = animItems[index];
       const animItemHeight = animItem.offsetHeight;
       const animItemOffset = offset(animItem).top;
       const animStart = 4;
-
       let animItemPoint = window.innerHeight - animItemHeight / animStart;
       if (animItemHeight > window.innerHeight) {
         animItemPoint = window.innerHeight - window.innerHeight / animStart;
       }
-
-      if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-        animItem.classList.add('_active');
+      if (
+        pageYOffset > animItemOffset - animItemPoint &&
+        pageYOffset < animItemOffset + animItemHeight
+      ) {
+        animItem.classList.add("_active");
       } else {
-        if (!animItem.classList.contains('_anim-no-hide')) {
-          animItem.classList.remove('_active');
+        if (!animItem.classList.contains("_anim-no-hide")) {
+          animItem.classList.remove("_active");
         }
       }
     }
@@ -160,7 +265,7 @@ if (animItems.length > 0) {
     const rect = el.getBoundingClientRect(),
       scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
       scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
   }
 
   setTimeout(() => {
@@ -169,17 +274,17 @@ if (animItems.length > 0) {
 }
 function menuSlider() {
   if (menuLinks.length > 0) {
-    menuLinks[pageSlider.realIndex].classList.add('_active');
+    menuLinks[pageSlider.realIndex].classList.add("_active");
     for (let index = 0; index < menuLinks.length; index++) {
       const menuLink = menuLinks[index];
-      menuLink.addEventListener('click', function (e) {
+      menuLink.addEventListener("click", function (e) {
         menuSliderRemove();
         pageSlider.slideTo(index, 800);
-        menuLink.classList.add('_active');
-        if (iconMenu.classList.contains('_active')) {
-          document.body.classList.remove('_lock');
-          iconMenu.classList.remove('_active');
-          menuBody.classList.remove('_active');
+        menuLink.classList.add("_active");
+        if (iconMenu.classList.contains("_active")) {
+          document.body.classList.remove("_lock");
+          iconMenu.classList.remove("_active");
+          menuBody.classList.remove("_active");
         }
         e.preventDefault();
       });
@@ -188,25 +293,25 @@ function menuSlider() {
 }
 
 function menuSliderRemove() {
-  let menuLinkActive = document.querySelector('.menu__link._active');
+  let menuLinkActive = document.querySelector(".menu__link._active");
   if (menuLinkActive) {
-    menuLinkActive.classList.remove('_active');
+    menuLinkActive.classList.remove("_active");
   }
 }
 
 function setScrollType() {
-  if (wrapper.classList.contains('_free')) {
-    wrapper.classList.remove('_free');
+  if (wrapper.classList.contains("_free")) {
+    wrapper.classList.remove("_free");
     pageSlider.params.freeMode.enabled = false;
   }
   for (let index = 0; index < pageSlider.slides.length; index++) {
     const pageSlide = pageSlider.slides[index];
-    const pageSlideContent = pageSlide.querySelector('.section__content');
+    const pageSlideContent = pageSlide.querySelector(".section__content");
 
     if (pageSlideContent) {
       const pageSlideContentHeight = pageSlideContent.offsetHeight;
       if (pageSlideContentHeight > window.innerHeight) {
-        wrapper.classList.add('_free');
+        wrapper.classList.add("_free");
         pageSlider.params.freeMode.enabled = true;
         break;
       }
@@ -216,15 +321,15 @@ function setScrollType() {
 
 const width = window.innerWidth;
 if (width < 768) {
-  let advSlider = new Swiper('.advantage__slider', {
-    slideClass: 'advantage__slider-slide',
+  let advSlider = new Swiper(".advantage__slider", {
+    slideClass: "advantage__slider-slide",
     grabCursor: true,
     pagination: {
-      el: '.advantages__pagination',
+      el: ".advantages__pagination",
       clickable: true,
-      type: 'bullets',
-      bulletClass: 'advantages__bullet',
-      bulletActiveClass: 'advantages__bullet_active',
+      type: "bullets",
+      bulletClass: "advantages__bullet",
+      bulletActiveClass: "advantages__bullet_active",
     },
     nested: true,
     simulateTouch: false,
@@ -240,159 +345,18 @@ if (width < 768) {
     breakpoints: {
       320: {
         slidesPerView: 1,
-        spaceBetween: 20
+        spaceBetween: 20,
       },
       480: {
         slidesPerView: 1.5,
-        spaceBetween: 30
+        spaceBetween: 30,
       },
       640: {
         slidesPerView: 2,
-        spaceBetween: 40
-      }
-    }
+        spaceBetween: 40,
+      },
+    },
   });
 }
-
-document.getElementById('form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const formData = Object.fromEntries(new FormData(e.target).entries());
-  console.log(formData);
-  form.reset();
-})
-
-
-// document.getElementById('form').addEventListener('submit', async function formSend(e) {
-//   e.preventDefault;
-
-//   let error = formValidate(form);
-
-//   let formData = new FormData(form);
-//   console.log(formData);
-//   if (error === 0) {
-//     form.classList.add('_sending');
-//     let response = await fetch('sendmail.php', {
-//       method: 'POST',
-//       body: formData,
-// Cache - Control: max - age=31536000
-//     });
-
-//     if (response.ok) {
-//       let result = await response.json();
-//       alert(result.message);
-//       form.reset();
-//       form.classList.remove('_sending');
-//     } else {
-//       alert('Error');
-//       form.classList.remove('_sending');
-//     }
-//   } else {
-//     alert('Fill required fields');
-//   }
-// })
-
-// function formValidate(form) {
-//   let error = 0;
-//   let formReq = document.querySelectorAll('._req');
-//   for (let index = 0; index < formReq.length; index++) {
-//     const input = formReq[index];
-//     formRemoveError(input);
-
-//     if (input.classList.contains('_email')) {
-//       if (emailTest(input)) {
-//         formAddError(input);
-//         error++;
-//       }
-//     } else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
-//       formAddError(input);
-//       error++;
-//     } else {
-//       if (input.value === '') {
-//         formAddError(input);
-//         error++;
-//       }
-//     }
-//   }
-//   return error;
-// }
-// function formAddError(input) {
-//   input.parentElement.classList.add('_error');
-//   input.classList.add('_error');
-// }
-// function formRemoveError(input) {
-//   input.parentElement.classList.remove('_error');
-//   input.classList.remove('_error');
-// }
-// function emailTest(input) {
-//   return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-// }
-
-
-// form.addEventListener('submit', formSend);
-
-// async function formSend(e) {
-//   e.preventDefault;
-
-//   let error = formValidate(form);
-
-//   let formData = new FormData(form);
-
-//   if (error === 0) {
-//     form.classList.add('_sending');
-//     let response = await fetch('sendmail.php', {
-//       method: 'POST',
-//       body: formData,
-//     });
-
-//     if (response.ok) {
-//       let result = await response.json();
-//       alert(result.message);
-//       form.reset();
-//       form.classList.remove('_sending');
-//     } else {
-//       // alert('Error');
-//       form.classList.remove('_sending');
-//     }
-//   } else {
-//     alert('Fill required fields');
-//   }
-// }
-// function formValidate(form) {
-//   let error = 0;
-//   let formReq = document.querySelectorAll('._req');
-//   for (let index = 0; index < formReq.length; index++) {
-//     const input = formReq[index];
-//     formRemoveError(input);
-
-//     if (input.classList.contains('_email')) {
-//       if (emailTest(input)) {
-//         formAddError(input);
-//         error++;
-//       }
-//     } else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
-//       formAddError(input);
-//       error++;
-//     } else {
-//       if (input.value === '') {
-//         formAddError(input);
-//         error++;
-//       }
-//     }
-//   }
-//   return error;
-// }
-// function formAddError(input) {
-//   input.parentElement.classList.add('_error');
-//   input.classList.add('_error');
-// }
-// function formRemoveError(input) {
-//   input.parentElement.classList.remove('_error');
-//   input.classList.remove('_error');
-// }
-// function emailTest(input) {
-//   return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-// }
-
-
 
 pageSlider.init();
--- sendmail.php
+++ sendmail.php
@@ -1,39 +1,32 @@
 <?php
 
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
 use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require '../vendor/autoload.php';
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
-
-//Create a new PHPMailer instance
 $mail = new PHPMailer(true);
-$mail->CharSet = 'UTF-8';
-$mail->setLanguage('en', 'phpmailer/language/');
+$mail->CharSet = 'utf-8';
 $mail->IsHTML(true);
 
-//Set PHPMailer to use the sendmail transport
-// $mail->isSendmail();
-//Set who the message is to be sent from
-$mail->setFrom('kateiiverem@gmail.com ', 'DAK Systems');
-//Set an alternative reply-to address
-// $mail->addReplyTo('replyto@example.com', 'First Last');
-//Set who the message is to be sent to
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'kateiiverem@gmail.com';
+$mail->Password = 'yrctzgqqskvelguo';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+
+// ivoryhealthy@belgianairways.com (test)
+$mail->addReplyTo($_POST['email'], $_POST['name']);
+$mail->setFrom('kateiiverem@gmail.com', 'DAK Systems');
 $mail->addAddress('info@compressor.by');
-//Set the subject line
-$mail->Subject = 'Request from DAK Systems_site';
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-// $mail->msgHTML(file_get_contents('contents.html'), __DIR__);
-//Replace the plain text body with one created manually
-// $mail->AltBody = 'This is a plain-text message body';
-//Attach an image file
-// $mail->addAttachment('images/phpmailer_mini.png');
+$mail->Subject = 'Request_DAK Systems';
 
 $body = '<h1>This message from site DAK Systems!</h1>';
-
 if (trim(!empty($_POST['name']))) {
   $body .= '<p><strong>Name:</strong> ' . $_POST['name'] . '</p>';
 }
@@ -46,7 +39,6 @@ if (trim(!empty($_POST['message']))) {
 
 $mail->Body = $body;
 
-//send the message, check for errors
 if (!$mail->send()) {
   $message = 'Error';
 } else {
